5901f82db835e37b85454a0b57e3ca84cd96585d
sagan <camjanssen120@gmail.com>
2020-08-15 12:05:51 -0700

    formatting and updating

--- src/main.rs
+++ src/main.rs
@@ -14,25 +14,7 @@ fn main() {
     let runs_list = get_runs_list(owner, repo).expect("Getting the runs list was unsuccessful");
     let run = runs_list.get_most_recent();
 
-    let jobs_list = get_jobs_list(owner, repo, run.unwrap().id).expect("Getting the jobs list was unsuccessful");
-
-    let current_job: Option<&Job> = jobs_list.jobs.iter()
-        .find(|j| matches!(j.identity.status, Status::InProgress));
-
-    if !jobs_list.jobs.iter().all(|j| matches!(j.identity.status, Status::Completed)) {
-
-        if current_job.is_some() {
-            println!("{}{}", get_jobs_list_string(&jobs_list.jobs), get_steps_list_string(&current_job.unwrap().steps));
-        } else {
-            println!("No current job found");
-        }
-
-        thread::sleep(Duration::from_secs(1));
-        main();
-
-    } else {
-        println!("Completed all jobs");
-    }
+    update(owner, repo, run.unwrap().id);
 }
 
 // gets the list of runs for this workflow
@@ -52,4 +34,27 @@ fn get_jobs_list(owner: &str, repo: &str, run_id: i64) -> Result<JobsList, serde
     let jobs_list: JobsList = serde_json::from_str::<JobsList>(jobs_list_string.as_str())?;
 
     Ok(jobs_list)
+}
+
+// repeatedly grabs updated info for the run of this id
+fn update(owner: &str, repo: &str, run_id: i64) {
+    let jobs_list = get_jobs_list(owner, repo, run_id).expect("Getting the jobs list was unsuccessful");
+
+    let current_job: Option<&Job> = jobs_list.jobs.iter()
+        .find(|j| matches!(j.identity.status, Status::InProgress));
+
+    if !jobs_list.jobs.iter().all(|j| matches!(j.identity.status, Status::Completed)) {
+
+        if current_job.is_some() {
+            print!("{}{}", get_jobs_list_string(&jobs_list.jobs), get_steps_list_string(&current_job.unwrap().steps));
+        } else {
+            println!("No current job found");
+        }
+
+        thread::sleep(Duration::from_millis(500));
+        update(owner, repo, run_id);
+
+    } else {
+        println!("Completed all jobs");
+    }
 }
